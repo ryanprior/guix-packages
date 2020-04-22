02c4dc9d18b918ebb3728f2e05a569e913a6cab4
Ryan Prior <ryanprior@gmail.com>
2020-04-22 15:30:57 -0500

    Updates VisiData package
    Improved with suggestions from Jack Hill <jackhill@jackhill.us>
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=40757#8

--- proposed/visidata.scm
+++ proposed/visidata.scm
@@ -1,4 +1,6 @@
-(define-module (visidata)
+;;; Copyright © 2020 Ryan Prior <rprior@protonmail.com>
+
+(define-module (gnu packages visidata)
   #:use-module (gnu packages databases)
   #:use-module (gnu packages python-science)
   #:use-module (gnu packages python-web)
@@ -7,7 +9,7 @@
   #:use-module (gnu packages xml)
   #:use-module (guix build-system python)
   #:use-module (guix download)
-  #:use-module (guix licenses)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages))
 
 (define-public visidata
@@ -16,14 +18,13 @@
    (version "1.5.2")
    (source (origin
             (method url-fetch)
-            (uri (string-append "https://github.com/saulpw/visidata/archive/v" version ".tar.gz"))
-            (sha256 (base32 "0h7hq6bnc8svkcc9995kkmgcb9n5qgm85rsshzzdicmg9rg3ymhi"))))
+            (uri (pypi-uri "visidata" version))
+            (sha256 (base32 "10adfyn4gkisvciqawgh2lakkhhnjjxiyp7mzbgcwkq1b3sigpf1"))))
    (build-system python-build-system)
-   (arguments '(#:tests? #f))
    ;; Tests disabled because they are not packaged with the source tarball.
-   ;; View test status here: https://circleci.com/gh/saulpw/visidata/tree/stable
    ;; Upstream suggests tests will be packaged with tarball around 2.0 release.
-   (native-inputs
+   (arguments '(#:tests? #f))
+   (inputs
     `(("python-dateutil" ,python-dateutil)
       ("python-fonttools" ,python-fonttools)
       ("python-h5py" ,python-h5py)
@@ -34,12 +35,13 @@
       ("python-requests" ,python-requests)
       ("python-xlrd" ,python-xlrd)
       ("python-pandas" ,python-pandas)))
-   (synopsis "Visidata: A terminal spreadsheet multitool for discovering and arranging data")
+   (synopsis "Terminal spreadsheet multitool for discovering and arranging data")
    (description
-    "VisiData is an interactive multitool for tabular data. It combines the
+    "VisiData is an interactive multitool for tabular data.  It combines the
 clarity of a spreadsheet, the efficiency of the terminal, and the power of
-Python, into a lightweight utility which can handle millions of rows with ease.")
+Python, into a lightweight utility which can handle millions of rows.")
    (home-page "https://www.visidata.org/")
-   (license gpl3)))
+   (license (list license:gpl3
+                  license:expat)))) ;; visidata/vdtui.py
 
 visidata
