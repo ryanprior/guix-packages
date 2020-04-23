275f0cb4ac28d7aef10bbaa2d87c24b5ce3bdeea
Ryan Prior <ryanprior@gmail.com>
2020-04-23 15:15:43 -0500

    Adds Hugo package
    It lacks necessary dependencies, which seems like it could be a decent chunk of
    work, so gonna just check this in for now.

---
+++ testing/hugo.scm
@@ -0,0 +1,35 @@
+;;; Copyright © 2020 Ryan Prior <rprior@protonmail.com>
+
+(define-module (gnu packages hugo)
+  #:use-module (guix build-system go)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages))
+
+(define-public hugo
+  (package
+    (name "hugo")
+    (version "0.69.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/gohugoio/hugo.git")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0wx13c3nck7zm1j6rhyksgsp1cv6mlmkwhkyijv153d117g7aslz"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/gohugoio/hugo"))
+    ;; There are a bunch of dependencies; see hugo-deps.org
+    ;; TODO figure out which deps are already packaged
+    ;; TODO package more go deps as necessary
+    (home-page "https://gohugo.io/")
+    (synopsis "Fast static site generator")
+    (description "Hugo takes a directory with content and templates and renders
+them into a full HTML website.")
+    (license license:asl2.0)))
+
+hugo
