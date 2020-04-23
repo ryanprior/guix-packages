edeadd29468ee3293e6f381273787c8a18f3a22f
Ryan Prior <ryanprior@gmail.com>
2020-04-22 23:41:16 -0500

    Updates Sideload package
    It works now!

--- testing/elementary.scm
+++ testing/elementary.scm
@@ -1,20 +1,20 @@
 (define-module (gnu packages elementary)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages)
+  #:use-module (guix build-system meson)
+  #:use-module (guix download)
   #:use-module (guix licenses)
   #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix utils)
-  #:use-module (guix build-system meson)
-  #:use-module (gnu packages)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages package-management)
-  #:use-module (gnu packages gettext)
-  #:use-module (gnu packages gnome)
-  #:use-module (gnu packages glib)
-  #:use-module (gnu packages xml)
-  #:use-module (gnu packages cmake)
-  #:use-module (gnu packages gtk))
-
-
+  #:use-module (guix utils))
 
 (define-public granite
   (package
@@ -30,12 +30,12 @@
      `(("gtk" ,gtk+)
        ("gtk+-bin" ,gtk+ "bin"))) ;; for gtk-update-icon-cache
     (native-inputs
-     `(("vala" ,vala)
+     `(("gettext" ,gettext-minimal)
        ("glib" ,glib)
        ("gobject-introspection" ,gobject-introspection)
        ("libgee" ,libgee)
        ("pkg-config" ,pkg-config)
-       ("gettext" ,gettext-minimal)))
+       ("vala" ,vala)))
     (home-page "https://github.com/elementary/granite")
     (synopsis "Library that extends GTK with common widgets and utilities")
     (description "Granite is a companion library for GTK+ and GLib. Among other
@@ -54,7 +54,11 @@ in apps built for elementary OS.")
               (sha256 (base32 "0i236hr3iflp4hp0fxczxdkj9d21gq75ai5s3a0z1djqzq4083z5"))))
     (build-system meson-build-system)
     (arguments
-     `(#:phases
+     `(#:glib-or-gtk? #t
+       #:configure-flags (list (string-append "-Dflatpak="
+                                              (assoc-ref %build-inputs "flatpak")
+                                              "/include"))
+       #:phases
        (modify-phases %standard-phases
          (add-after 'install 'install-symlinks
            (lambda* (#:key outputs #:allow-other-keys)
@@ -63,16 +67,23 @@ in apps built for elementary OS.")
                     (link (string-append out "/bin/sideload")))
                (symlink bin link)))))))
     (inputs
-     `(("gtk" ,gtk+)
-       ("granite" ,granite)))
+     `(("flatpak" ,flatpak)
+       ("granite" ,granite)
+       ("gtk" ,gtk+)
+       ("libostree" ,libostree)))
+    (propagated-inputs
+     `(("glib-networking" ,glib-networking)))
     (native-inputs
-     `(("vala" ,vala)
-       ("glib" ,glib "bin")
-       ("pkg-config" ,pkg-config)
-       ("cmake" ,cmake)
-       ("flatpak" ,flatpak)
+     `(("cmake" ,cmake)
+       ("desktop-file-utils" ,desktop-file-utils) ; for update-desktop-database
+       ("gettext" ,gettext-minimal)
+       ("glib" ,glib)
+       ("glib:bin" ,glib "bin")
+       ("gobject-introspection" ,gobject-introspection)
+       ("libgee" ,libgee)
        ("libxml2" ,libxml2)
-       ("gettext" ,gettext-minimal)))
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
     (home-page "https://github.com/elementary/sideload")
     (synopsis "Graphical application to side-load Flatpaks")
     (description "Sideload handles flatpakref files, like those you might find
