3cdb04dd0120ae6895ca3c6cddac9e3987b99243
Ryan Prior <ryanprior@gmail.com>
2020-04-22 22:00:43 -0500

    Updates Harvey package
    It works now!

--- testing/harvey.scm
+++ testing/harvey.scm
@@ -5,6 +5,8 @@
   #:use-module (guix utils)
   #:use-module (guix build-system meson)
   #:use-module (gnu packages)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages gettext)
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gtk))
@@ -15,23 +17,30 @@
     (version "1.0.0")
     (source (origin
               (method url-fetch)
-              (uri (string-append "https://github.com/danrabbit/harvey"
-                                  "/archive/"
+              (uri (string-append "https://github.com/danrabbit/harvey/archive/"
                                   version ".tar.gz"))
-              (sha256
-               (base32
-                "0n57h4541v2vdphwb87qzn3k2p7ppyzkbdnw2fkhbd7awvcfbn2i"))))
+              (sha256 (base32 "0n57h4541v2vdphwb87qzn3k2p7ppyzkbdnw2fkhbd7awvcfbn2i"))))
     (build-system meson-build-system)
-    (inputs
-     `(("vala" ,vala)
-       ("gtk" ,gtk+)))
-    (native-inputs
-     `(("glib" ,glib "bin")))
     (arguments
-     `(#:tests? #f))
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'install 'install-symlinks
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (bin (string-append out "/bin/com.github.danrabbit.harvey"))
+                    (link (string-append out "/bin/harvey")))
+               (symlink bin link)))))))
+    (inputs
+     `(("gtk" ,gtk+)))
+    (native-inputs
+     `(("vala" ,vala)
+       ("glib" ,glib "bin")
+       ("pkg-config" ,pkg-config)
+       ("gettext" ,gettext-minimal)))
     (home-page "https://appcenter.elementary.io/com.github.danrabbit.harvey/")
     (synopsis "Calculate and visualize color contrast")
-    (description "The hero that Gotham needs right now.
-Harvey calculates and visualizes color contrast.
+    (description "Harvey calculates and visualizes color contrast.
 It checks a given set of colors for WCAG contrast compliance.")
     (license gpl2+)))
+
+harvey
