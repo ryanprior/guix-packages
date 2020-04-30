b72d381c8b33221c332563b2c54f2e9968559f83
Ryan Prior <ryanprior@gmail.com>
2020-04-30 01:04:47 -0500

    Adds pantheon-calculator package
    It can't find granite during the configure stage, though.

--- testing/elementary.scm
+++ testing/elementary.scm
@@ -150,6 +150,38 @@ we interact with the software repositories provided by GNU/Linux distributions
 by standardizing software component metadata.")
     (license license:gpl2)))
 
+(define-public pantheon-calculator
+  (package
+    (name "pantheon-calculator")
+    (version "1.5.5")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/elementary/calculator.git")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1csxsr2c8qvl97xz9ahwn91z095nzgr0i1mbcb1spljll2sr9lkj"))))
+    (build-system meson-build-system)
+    (arguments '(#:glib-or-gtk? #t))
+    (inputs
+     `(("granite" ,granite)
+       ("gtk" ,gtk+)))
+    (native-inputs
+     `(("glib" ,glib)
+       ("cmake" ,cmake)
+       ("gettext" ,gettext-minimal)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "https://github.com/elementary/calculator")
+    (synopsis "Desktop calculator")
+    (description "Calculator is an application for performing simple arithmatic.
+It is the default calculator application in the Pantheon desktop
+environment.")
+    (license license:gpl3)))
+
 (define-public pantheon-terminal
   (package
     (name "pantheon-terminal")
