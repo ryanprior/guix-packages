630c307e99b7ddf0537c48e67a8ba8911199cb88
Ryan Prior <ryanprior@gmail.com>
2020-05-17 13:52:49 -0500

    Adds emacs-importmagic

--- testing/importmagic.scm
+++ testing/importmagic.scm
@@ -74,4 +74,26 @@ elisp.")
 finding unresolved symbols in Python code and their corresponding imports.")
     (license license:bsd-3)))
 
-python-importmagic
+(define-public emacs-importmagic
+  (package
+    (name "emacs-importmagic")
+    (version "20180520.303")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        (string-append "https://melpa.org/packages/importmagic-" version ".tar"))
+       (sha256
+        (base32 "0xk4i4x4836ksv2pr3aarpbkq6b5sz8c3y6f39fwf698v8zirhs9"))))
+    (build-system emacs-build-system)
+    (propagated-inputs
+     `(("emacs-epc" ,emacs-epc)
+       ("emacs-f" ,emacs-f)
+       ("python-epc" ,python-epc)
+       ("python-importmagic" ,python-importmagic)))
+    (home-page "https://github.com/anachronic/importmagic.el")
+    (synopsis "Fix Python imports")
+    (description "Importmagic.el fixes unresolved imports in Python buffers.")
+    (license license:gpl3)))
+
+emacs-importmagic
