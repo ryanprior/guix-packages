17d3b1ae3bb326e34a6c09b226a6d6502a8c4996
Ryan Prior <ryanprior@gmail.com>
2020-05-19 23:57:12 -0500

    Adds wrap-cc experiment

---
+++ experiments/wrap-cc.scm
@@ -0,0 +1,40 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2020 Ryan Prior <rprior@protonmail.com>
+
+(define-module (experiments wrap-cc)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages c)
+  #:use-module (gnu packages gcc)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix build utils)
+  #:use-module (guix utils)
+  #:use-module (guix packages))
+
+(define* (wrap-cc cc bin name)
+                  ;; (bin (package-name cc))
+                  ;; (name (string-append (package-name cc) "-wrapper")))
+  (package/inherit cc
+    (name name)
+    (source #f)
+    (build-system trivial-build-system)
+    (outputs '("out"))
+    (propagated-inputs `(("cc" ,cc)))
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin
+         (use-modules (guix build utils))
+         (let ((bin-dir (string-append (assoc-ref %outputs "out") "/bin/")))
+           (symlink (string-append bin-dir ,bin)
+                    (string-append bin-dir "cc"))))))
+    (synopsis "Wrapper for c compilers")
+    (description "This package provides wrappers for c compilers such that they
+can be invoked under the name @command{cc}.")))
+
+(define-public gcc-wrapper (wrap-cc gcc-toolchain "gcc" "gcc-wrapper"))
+;; (define-public gcc-wrapper (wrap-cc gcc))
+;; (define-public tcc-wrapper (wrap-cc tcc))
+
+gcc-wrapper
