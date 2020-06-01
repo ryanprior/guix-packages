b196ddae1c56bd5f14a0695195c8cd5bffb71729
Ryan Prior <ryanprior@gmail.com>
2020-05-31 23:50:24 -0500

    Adds package vala-language-server to testing

--- testing/vala-language-server.scm
+++ testing/vala-language-server.scm
@@ -62,3 +62,33 @@ language features to GNOME developers without imposing any additional runtime
 requirements and without using a different ABI compared to applications and
 libraries written in C.")
     (license license:lgpl2.1+)))
+
+(define-public vala-language-server
+  (package
+    (name "vala-language-server")
+    (version "0.48")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/benwaffle/vala-language-server.git")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "0chgfpci247skrvsiq1l8cas8sj2z6z42dlarka3df3qwxmh0if0"))))
+    (build-system meson-build-system)
+    (arguments '(#:glib-or-gtk? #t))
+    (inputs
+     `(("jsonrpc-glib" ,jsonrpc-glib)
+       ("libgee" ,libgee)
+       ("json-glib" ,json-glib)))
+    (native-inputs
+     `(("glib" ,glib)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala-0.48)))
+    (home-page "https://github.com/benwaffle/vala-language-server")
+    (synopsis "Language server for Vala")
+    (description "The Vala language server is an implementation of the Vala
+language specification for the Language Server Protocol (LSP).  This tool is
+used in text editing environments to provide a complete and integrated
+feature-set for programming Vala effectively.")
+    (license license:lgpl2.1+)))
