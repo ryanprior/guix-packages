17124a284e74e6a461e34818421c445d8e2fc9ee
Ryan Prior <ryanprior@gmail.com>
2020-06-04 18:04:30 -0500

    Adds pantheon-icon-theme
    Imported from lprndn's tree with my modifications.

--- testing/pantheon.scm
+++ testing/pantheon.scm
@@ -38,3 +38,35 @@
     (description "Theme for the Pantheon desktop environment using a custom
 designed GTK-CSS stylesheet.")
     (license license:gpl3+)))
+
+(define-public pantheon-icon-theme
+  (package
+    (name "pantheon-icon-theme")
+    (version "5.3.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/elementary/icons.git")
+                    (commit version)))
+              (sha256
+               (base32 "0rs68cb39r9vq85pr8h3mgmyjpj8bkhkxr5cz4cn5947kf776wg9"))))
+    (build-system meson-build-system)
+    (arguments
+     `(#:configure-flags
+       (list "-Dvolume_icons=false")))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)
+       ("gettext" ,gettext-minimal)
+       ("inkscape" ,inkscape)
+       ("xcursorgen" ,xcursorgen)
+       ("librsvg" ,librsvg)))
+    (inputs
+     `(("gtk+" ,gtk+)))
+    (propagated-inputs
+     `(("hicolor-icon-theme" ,hicolor-icon-theme)))
+    (home-page "https://github.com/elementary/icons")
+    (synopsis "Icons for Pantheon")
+    (description "Vector icon theme for Pantheon.  It is not a comprehensive
+universal icon set; its coverage is tailored for the software available in
+elementary OS, for which Pantheon is the first-party desktop environment.")
+    (license license:gpl3+)))
