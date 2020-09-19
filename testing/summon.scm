23881d1f4af8ed0b02f5306f6f67754417b1ce10
Ryan Prior <ryanprior@hey.com>
2020-09-19 14:34:32 -0500

    Adds go-github.com-karrick-golf

--- testing/summon.scm
+++ testing/summon.scm
@@ -32,3 +32,27 @@
     (arguments
      '(#:import-path "gopkg.in/yaml.v3"))
     (home-page "https://gopkg.in/yaml.v3")))
+
+(define-public go-github-com-karrick-golf
+  (package
+    (name "go-github.com-karrick-golf")
+    (version "1.4.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/karrick/golf")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "05gfgajyzjnrqp9c9cy400h8w3hps9kssc5szscp06117z2nb1sp"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/karrick/golf"))
+    (home-page "https://github.com/karrick/golf")
+    (synopsis "Light-weight long and short command line option parser")
+    (description
+     "Golf is a modest options parsing library for Go command line interface
+programs.  Meant to be small, like flag included in Go's standard library.")
+    (license license:asl2.0)))
