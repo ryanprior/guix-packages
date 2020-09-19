cb04fe6c992ad0b4cccfa082c2913b888e3101d3
Ryan Prior <ryanprior@hey.com>
2020-09-19 14:44:19 -0500

    Adds go-github.com-cyberark-conjur-api

--- testing/summon.scm
+++ testing/summon.scm
@@ -1,5 +1,6 @@
 (define-module (testing summon)
   #:use-module (gnu packages)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages golang)
   #:use-module (guix build-system go)
   #:use-module (guix git-download)
@@ -98,3 +99,33 @@ present in the source file.")
        (sha256
         (base32
          "1zf9is1yxxnna0d1pyag2m9ziy3l27zb2j92p9msm1gx5jjrvzzj"))))))
+
+(define-public go-github-com-cyberark-conjur-api
+  (package
+    (name "go-github.com-cyberark-conjur-api")
+    (version "0.6.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/cyberark/conjur-api-go")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1w3f82miwh280sq5izfbmrr7vnwfjg8vr9lhc5m8b9yvb0cvi83j"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:tests? #f ;; conjur-api-go has tests but it requires a network
+                   ;; service to run them.
+       #:import-path "github.com/cyberark/conjur-api-go/conjurapi"
+       #:unpack-path "github.com/cyberark/conjur-api-go"))
+    (inputs
+     `(("github.com/smartystreets/goconvey" ,go-github.com-smartystreets-goconvey)))
+    (propagated-inputs
+     `(("github.com/bgentry/go-netrc" ,go-github-com-bgentry-go-netrc)
+       ("github.com/sirupsen/logrus" ,go-github-com-sirupsen-logrus-1.6)))
+    (home-page "https://github.com/cyberark/conjur-api-go")
+    (synopsis "Go client for the CyberArk Conjur API ")
+    (description "This package provides programmatic Golang access to the Conjur API.")
+    (license license:asl2.0)))
