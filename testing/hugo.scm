44239a15acfbb93e5a1a2c1812d9eb06bddd8995
Ryan Prior <ryanprior@hey.com>
2020-09-24 23:16:12 -0500

    Adds go-github-com-mitchellh-hashstructure

--- testing/hugo.scm
+++ testing/hugo.scm
@@ -663,6 +663,32 @@ variables.")
 capitalizations, spacing, and so on.")
     (license license:expat)))
 
+(define-public go-github-com-mitchellh-hashstructure
+  (package
+    (name "go-github-com-mitchellh-hashstructure")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/mitchellh/hashstructure")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0zgl5c03ip2yzkb9b7fq9ml08i7j8prgd46ha1fcg8c6r7k9xl3i"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/mitchellh/hashstructure"))
+    (home-page "https://github.com/mitchellh/hashstructure")
+    (synopsis "Get hash values for arbitrary values in golang.")
+    (description
+     "This package can be used to key values in a hash (for use in a map, set,
+etc.) that are complex.  The most common use case is comparing two values
+without sending data across the network, caching values locally (de-dup), and
+so on.")
+    (license license:expat)))
+
 (define-public hugo
   (package
     (name "hugo")
