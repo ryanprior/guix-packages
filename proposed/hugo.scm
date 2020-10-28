e4f9d9ce6c2150e2b7e9efd877571fed6cc3f8ee
Ryan Prior <ryanprior@hey.com>
2020-10-27 21:35:02 -0500

    Removes esbuild from proposed

--- proposed/hugo.scm
+++ proposed/hugo.scm
@@ -9,30 +9,4 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages))
 
-(define-public esbuild
-  (package
-    (name "esbuild")
-    (version "0.7.11")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/evanw/esbuild")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "08lx1i0s1q3my6x4csz746rc3c2fl4p0hyrwx4rpx80gwwjj1i11"))))
-    (build-system go-build-system)
-    (arguments
-     '(#:import-path "github.com/evanw/esbuild/cmd/esbuild"
-       #:unpack-path "github.com/evanw/esbuild"))
-    (inputs
-     `(("go-golang-org-x-sys" ,go-golang-org-x-sys)))
-    (home-page "https://github.com/evanw/esbuild")
-    (synopsis "Bundler and minifier tool for JavaScript and TypeScript")
-    (description
-     "The esbuild tool provides a unified bundler, transpiler and
-minifier.  It packages up JavaScript and TypeScript code, along with JSON
-and other data, for distribution on the web.")
-    (license license:expat)))
+
