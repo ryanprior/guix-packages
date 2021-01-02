7be07e9f090a2a34e2c1aa995a11987266677762
Ryan Prior <ryanprior@hey.com>
2021-01-01 23:56:17 -0600

    Adds wyhash

--- proposed/vlang.scm
+++ proposed/vlang.scm
@@ -34,3 +34,37 @@
       (sha256
        (base32 "0s982qiwy4s9y07x9fsy4yn642schplhp9hrw2libg2bx4sw43as")))))
 
+(define-public wyhash
+  (package
+    (name "wyhash")
+    (version "5")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/wangyi-fudan/wyhash")
+                    (commit (string-append "wyhash_v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "03ljs5iw9zrm3bydwggjvpwrcwmsd75h3dv1j4am4hw3h22cjdjc"))))
+    (build-system trivial-build-system) ;; source-only package
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin
+         (use-modules (guix build utils))
+         (let* ((out (string-append (assoc-ref %outputs "out")))
+                (src (string-append out "/include"))
+                (doc (string-append out "/share/doc/" ,name "-" ,version)))
+           (mkdir-p src)
+           (mkdir-p doc)
+           (chdir (assoc-ref %build-inputs "source"))
+           (install-file "wyhash.h" src)
+           (install-file "LICENSE" doc)
+           (install-file "README.md" doc))
+         #t)))
+    (home-page "https://github.com/wangyi-fudan/wyhash")
+    (synopsis "Embeddable hash function and random number generator.")
+    (description "This package provides a portable hash function and random
+number generator suitable for use in data structures.  Provided by default in
+Zig, V, and Nim programming language standard libraries.")
+    (license license:unlicense)))
